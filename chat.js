5aaba3b72f7b2bf27254c7fbcd20f43af99c060c
RVovk7 <RVovk7@gmail.com>
2018-01-28 18:17:39 +0200

    beta 0,1

--- chat.js
+++ chat.js
@@ -1,51 +1,66 @@
 'use strict';
-let xnr = new XMLHttpRequest();
+let xnrUser = new XMLHttpRequest();
 addUser.addEventListener('click', () => {
-    xnr.open('GET', 'https://randomuser.me/api');
-    xnr.onreadystatechange = () => {
-        if (xnr.status == 200 && xnr.readyState === 4) {
-            let arrUsers = JSON.parse(xnr.responseText);
+    xnrUser.open('GET', 'https://randomuser.me/api');
+    xnrUser.onreadystatechange = () => {
+        if (xnrUser.status == 200 && xnrUser.readyState === 4) {
+            let arrUsers = JSON.parse(xnrUser.responseText);
 
             UI(arrUsers.results[0]);
 
 
         }
     }
-    xnr.send();
+    xnrUser.send();
 });
 
-let arr = [];
+let xnrMessage = new XMLHttpRequest();
+
+function getM(a){
+    xnrMessage.open('GET', 'http://www.randomtext.me/api/gibberish/p-5/5-15');
+    xnrMessage.onreadystatechange = () => {
+        if (xnrMessage.status == 200 && xnrMessage.readyState === 4) {
+            let arrMessage = JSON.parse(xnrMessage.responseText);
+             message(a,arrMessage['text_out']);
+        }
+    }
+    xnrMessage.send();
+} 
+let arrUsr = [];
 function UI(user) {
-  
+
     let userMain = document.getElementById('users');
     userMain.scrollTop = userMain.scrollHeight;
-    let userDiv =document.createElement('div');
+    let userDiv = document.createElement('div');
     userDiv.classList.add('userDiv');
     let name = user['name'];
     let foto = user['picture'];
     let avatar = document.createElement('div');
     avatar.classList.add("avatar");
-    
+
     let img = document.createElement("IMG");
     img.src = foto['medium'];
     let userInf = document.createElement('div');
     userInf.classList.add('userInf');
-    userInf.innerHTML = `${name['last'][0].toUpperCase() + name['last'].slice(1,name['last'].length) } 
-    ${name['first'][0].toUpperCase() + name['first'].slice(1,name['first'].length)}`;
+    userInf.innerHTML = `${name['last'][0].toUpperCase() + name['last'].slice(1, name['last'].length)} 
+    ${name['first'][0].toUpperCase() + name['first'].slice(1, name['first'].length)}`;
 
-   let userAbout = document.createElement('div');
-   userAbout.classList.add('userAbout');
-   let city = user['location'];
-userAbout.innerHTML = `City:${city['city'][0].toUpperCase() + city['city'].slice(1,city.length)}<br> street:${city['street'] }
- <br>Phone:${user['cell']}`
- 
+    let userAbout = document.createElement('div');
+    userAbout.classList.add('userAbout');
+    let city = user['location'];
+    userAbout.innerHTML = `City:${city['city'][0].toUpperCase() + city['city'].slice(1, city.length)}<br> Street:${city['street']}
+    <br>Phone:${user['cell']}`
 
-avatar.appendChild(img);
+
+    avatar.appendChild(img);
     userDiv.appendChild(avatar);
     userInf.appendChild(userAbout);
     userDiv.appendChild(userInf);
     userMain.appendChild(userDiv);
-    arr.push(user);
-    console.log(arr);
-    console.log(user['name']);
+    arrUsr.push(user);
+    getM(arrUsr);
+}
+function message(users,mess){
+  
+console.log(mess);
 }
